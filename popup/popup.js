1563d6dd87fb3ae8d22104d395ccf19b0029801a
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2021-05-16 17:51:43 -0300

    primeira versão

---
+++ popup/popup.js
@@ -0,0 +1,35 @@
+let contador;
+let firulas = []
+if(localStorage.length>0)
+{
+    contador = localStorage.length;
+    for (let index = 1; index <= contador; index++) {
+        firulas.push(localStorage.getItem("k"+index))
+    }
+    EscreverFirulas()
+}
+else
+{
+    contador = 1;
+}
+
+document.querySelector("#input").addEventListener("keydown",function(a){
+    if(a.code == "Enter" || a.code == "NumpadEnter")
+    {
+        document.querySelector("#firulas").innerHTML+= `<p>${document.querySelector("#input").value}</p>`;
+        localStorage.setItem("k"+contador,document.querySelector("#input").value)
+        contador++;
+        document.querySelector("#input").value = "";
+    }
+});
+function EscreverFirulas()
+{
+    firulas.forEach(function(a) {
+        document.querySelector("#firulas").innerHTML+= `<p id="k">${a}</p>`;
+    });
+}
+
+document.querySelector("#apagar").addEventListener("click",function(){
+    localStorage.clear()
+    document.querySelector("#firulas").innerHTML = ""
+})
